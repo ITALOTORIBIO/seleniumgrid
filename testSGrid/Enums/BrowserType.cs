bc9c0c34b81c9f66dec1127f708190c779584821
ITALOTORIBIO <italo_toribio@usmp.pe>
2022-02-15 07:36:32 -0800

    docker ok

---
+++ testSGrid/Enums/BrowserType.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+ 
+namespace testSGrid.Enums
+{
+    public enum BrowserType
+    {
+        Chrome,
+        Edge,
+        Firefox,
+        Opera
+    }
+}
