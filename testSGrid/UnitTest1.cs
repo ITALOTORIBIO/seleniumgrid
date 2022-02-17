38d24ead16c50e75703d4330ee7c18a570873ca9
juancarlosTudela <juanktudela@gmail.com>
2022-02-17 10:48:09 -0500

    Allure Report + Docker

--- testSGrid/UnitTest1.cs
+++ testSGrid/UnitTest1.cs
@@ -54,11 +54,11 @@ public class Tests
     {
         driver.Navigate().GoToUrl("https://www.google.com");
         driver.Manage().Window.Maximize();
-        driver.FindElement(By.Name("q")).SendKeys("I Want to see this on a remote machine");
+        driver.FindElement(By.Name("q")).SendKeys("I Want to se this on a remote machine");
         string text = driver.FindElement(By.XPath("//a[@class='spell_orig']")).Text;
         AllureLifecycle.Instance.WrapInStep(() =>
         {
-            Assert.Equals("I Want to see this on a remote machine", text);
+            Assert.Equals("I Want to se this on a remote machine", text);
         }, "Google Search");
     }
 
