1994d015b6d0b10c2626137f49d33e344a134118
ITALOTORIBIO <italo_toribio@usmp.pe>
2022-02-18 00:42:33 -0500

    Italo test

--- testSGrid/UnitTest1.cs
+++ testSGrid/UnitTest1.cs
@@ -11,71 +11,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using NUnit.Allure.Core;
-using Allure.Commons;
-using NUnit.Allure.Attributes;
-
 namespace testSGrid;
-[TestFixture]
-[AllureNUnit]
 
 public class Tests
 {
     private IWebDriver driver;
-    string hubUrl;
-    public IDictionary<string, object> vars { get; private set; }
-    private IJavaScriptExecutor js;
+        string hubUrl;
+        public IDictionary<string, object> vars { get; private set; }
+        private IJavaScriptExecutor js;
 
-    [SetUp]
-    public void Setup()
-    {
-        AllureLifecycle.Instance.CleanupResultDirectory();
-        vars = new Dictionary<string, object>();
-
-        hubUrl = "http://localhost:4444/wd/hub";
-        driver = LocalDriverFactory.CreateInstance(Enums.BrowserType.Chrome, hubUrl);
-        js = (IJavaScriptExecutor)driver;
-    }
-
-    [TearDown]
-    protected void TearDown()
-    {
-        driver.Quit();
-    }
-
-    [Test]
-    [Parallelizable]
-    [Category("SampleTag")]
-    [Description("Test1")]
-    [AllureTag("NUnit", "Debug")]
-    [AllureSeverity(SeverityLevel.critical)]
-    [AllureFeature("Core")]
-    public void OpenGoogleAndSearch()
-    {
-        driver.Navigate().GoToUrl("https://www.google.com");
-        driver.Manage().Window.Maximize();
-        driver.FindElement(By.Name("q")).SendKeys("I Want to see this on a remote machine");
-        string text = driver.FindElement(By.XPath("//a[@class='spell_orig']")).Text;
-        AllureLifecycle.Instance.WrapInStep(() =>
+        [SetUp]
+        public void Setup()
         {
-            Assert.Equals("I Want to see this on a remote machine", text);
-        }, "Google Search");
-    }
+            vars = new Dictionary<string, object>();
 
-    /*[Test]
-    [Parallelizable]
-    [Category("SampleTag")]
-    [Description("Test2")]
-    [AllureTag("NUnit", "Debug")]
-    [AllureSeverity(SeverityLevel.critical)]
-    [AllureFeature("Core")]
-    public void OpenBingAndSearch()
-    {
-        driver.Navigate().GoToUrl("https://www.bing.com/");
-        driver.Manage().Window.Maximize();
-        driver.FindElement(By.Name("q")).SendKeys("I Want to see this on a remote machine");
-    }*/
+            hubUrl = "http://localhost:4444/wd/hub";
+            driver = LocalDriverFactory.CreateInstance(Enums.BrowserType.Chrome, hubUrl);
+            js = (IJavaScriptExecutor)driver;
+        }
+
+        [TearDown]
+        protected void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        [Parallelizable]
+        public void OpenGoogleAndSearch()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("q")).SendKeys("I Want to se this on a remote machine");
+        }
+
+        [Test]
+        [Parallelizable]
+        public void OpenBingAndSearch()
+        {
+            driver.Navigate().GoToUrl("https://www.bing.com/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("q")).SendKeys("I Want to seee this on a remote machine");
+        }
 
 
-
-}
+} 
